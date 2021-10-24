de9d53887558670d9f7d17da1cea53b159ba41af
mrslv-bss <bassmiroslav@gmail.com>
2021-10-24 20:36:33 +0300

    HomePage

---
+++ src/main/java/com/w2a/pages/HomePage.java
@@ -0,0 +1,41 @@
+package com.w2a.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+	WebDriver driver;
+	
+	public void goToSupport(){
+		
+	}
+	
+	public void goToLogin(){
+		
+		driver.findElement(By.cssSelector(".signup")).click();
+		
+	}
+	
+	public void goToSignUp(){
+		
+	}
+	
+	public void goToZoho(){
+		
+	}
+	
+	public void goToLearnMore(){
+		
+	}
+	
+	public void validateFooterLinks(){
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
