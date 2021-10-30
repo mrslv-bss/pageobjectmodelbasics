78508af552acf6e81447f0c6de79817066b59389
mrslv-bss <bassmiroslav@gmail.com>
2021-10-30 16:24:36 +0300

    upd: geckodriver

--- src/test/java/com/w2a/rough/LoginTest.java
+++ src/test/java/com/w2a/rough/LoginTest.java
@@ -3,8 +3,8 @@ package com.w2a.rough;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.w2a.pages.HomePage;
 
@@ -12,10 +12,10 @@ public class LoginTest {
 
 	public static void main(String[] args) {
 
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\com\\w2a\\executables\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-//		System.setProperty("webdriver.firefox.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\com\\w2a\\executables\\geckodriver.exe");
-//		WebDriver driver = new FirefoxDriver();
+//		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\com\\w2a\\executables\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.firefox.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\com\\w2a\\executables\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.zoho.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
